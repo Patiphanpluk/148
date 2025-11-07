764b839985ead180d9451391543c97b6b9b915b5
Patiphanpluk <108994826+Patiphanpluk@users.noreply.github.com>
2025-11-08 03:29:42 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,6 @@
-// ✅ นำเข้า Firebase SDK เวอร์ชันที่เหมาะกับเว็บ static (ไม่มี build)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
 
-// 🔥 ตั้งค่าด้วยค่าของคุณจาก Firebase Console
 const firebaseConfig = {
   apiKey: "AIzaSyDM86DKKFNuulKsKEAFqEM1F-XzosBuVx4",
   authDomain: "dynamic-qr-afbae.firebaseapp.com",
@@ -13,9 +11,7 @@ const firebaseConfig = {
   measurementId: "G-52KCB2KR05"
 };
 
-// ✅ เริ่มการเชื่อมต่อ Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// ✅ ส่งออกฐานข้อมูลให้ไฟล์อื่นใช้
 export { db };
