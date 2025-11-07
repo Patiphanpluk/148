7d6ee0ff1a131a145d3adaf2bfe0a9c9a6f1e1fb
Patiphanpluk <108994826+Patiphanpluk@users.noreply.github.com>
2025-11-08 03:08:39 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID"
+  apiKey: "AIzaSyDM86DKKFNuulKsKEAFqEM1F-XzosBuVx4",
+  authDomain: "dynamic-qr-afbae.firebaseapp.com",
+  projectId: "dynamic-qr-afbae",
+  storageBucket: "dynamic-qr-afbae.firebasestorage.app",
+  messagingSenderId: "589650390205",
+  appId: "1:589650390205:web:c8eb14a74a34da34d99bd4",
+  measurementId: "G-52KCB2KR05"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
